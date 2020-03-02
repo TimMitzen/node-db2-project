e53865897c98da7df9d0ce7e234fbe41fcbdcbf9
timislearning <tim.mitzen@gmail.com>
2020-03-02 10:01:04 -0800

    mvp and some strech

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,13 @@
 
 module.exports = {
 
-  development: {
+  
     client: 'sqlite3',
     connection: {
       filename: './data/database_file.db3'
     },
     useNullAsDefault: true
-  },
+  
 
   
 
